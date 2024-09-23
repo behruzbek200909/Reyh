f38cef179f8d16bac6206f9063a9b315491508df
behruzbek200909 <behruzbekmahkamov94@gmail.com>
2024-09-23 16:26:33 +0600

    Reyh

---
+++ index.js
@@ -0,0 +1,19 @@
+////////////// number
+
+var a = 1934;
+
+///////////// string
+
+var str = "muhmadali web dasturchi Samarqanlik 70%"
+
+//////////// boolean
+
+var Studen = true;
+
+//////////// undifined
+
+var notFound;
+
+/////////////// null
+
+var emty = null;
---
+++ index.ts
@@ -0,0 +1,35 @@
+///////////////number
+
+let a: number = 1112
+
+/////////////// string
+
+let str: string = "muhamali"
+
+///////////////boolean
+
+let isStudent: boolean = true
+
+/////////////// underfined
+
+let notFound: undefined
+
+///////////// null
+
+let emty: null = null
+
+let arr:[number, string , boolean] = [19, "salom", false]
+
+////////////// Symbol
+
+interface IObject {
+    name: string; 
+    age: number;
+}
+
+/////////////////////// object
+
+let obj:IObject = {
+    name: "Muhamadali",
+    age: 19
+}
